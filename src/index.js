a18ad7f80e233a5360de6b67e1f47ccb9d92f94d
cuiss <css@ect888.com>
2021-03-05 17:58:57 +0800

    多model配置

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import dva from "dva";
 import "./assets/css/base.less";
 import "./utils/layout.js";
+
 // import "antd/dist/antd.less";
+
+import { browserHistory } from 'dva/router';
+// import createLoading from 'dva-loading';
+import { Toast } from 'antd-mobile';
+
+const ERROR_MSG_DURATION = 3; // 3 秒
+
+
 // 1. Initialize
-const app = dva();
+const app = dva({
+  history: browserHistory,
+  onError(e) {
+    Toast.fail(e.message, ERROR_MSG_DURATION);
+  },
+});
 
 // 2. Plugins
-// app.use({});
+// app.use(createLoading());
 
 // 3. Model
-app.model(require("./models/counter").default);
+// app.model(require("./models/counter").default);
+// app.model(
+//   require("./models").default.forEach((key) =>
+//     app.model(require(`./models/${key}`).default)
+//   )
+// );
+
+require("./models").default.forEach((key) => app.model(key.default));
 
 // 4. Router
 app.router(require("./router").default);
