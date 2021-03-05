2246fd5fca02509e0eccfe1abee2c2561dfc1cb5
cuiss <css@ect888.com>
2021-03-05 17:59:23 +0800

    动态路由配置

--- src/router.js
+++ src/router.js
@@ -1,58 +1,104 @@
 import React, { useEffect } from "react";
 import { Router, Route, Switch } from "dva/router";
 
-// const ZyHome = lazy(() => import('./components/ZyHome/ZyHome'));
-// const ZyFund = lazy(() => import('./components/ZyFund/ZyFund'));
-// const ZyCapital = lazy(() => import('./components/ZyCapital/ZyCapital'));
-// const ZyMine = lazy(() => import('./components/ZyMine/ZyMine'));
-// const NotFound = lazy(() => import('./components/NotFound/NotFound'));
+// 异步模块加载
+import dynamic from "dva/dynamic";
 
-import ZyHome from "./components/ZyHome/ZyHome";
-import ZyFund from "./components/ZyFund/ZyFund";
-import ZyCapital from "./components/ZyCapital/ZyCapital";
-import ZyMine from "./components/ZyMine/ZyMine";
-import NotFound from "./components/NotFound/NotFound";
-import Login from "./components/Login/Login";
+// import Counter from "./routes/Counter";
+// import Main from "./routes/Main";
 
-const routerList = [
-  {
-    title: "首页",
-    component: ZyHome,
-    path: "/zyhome",
-    exact: true,
-  },
-  {
-    title: "基金",
-    component: ZyFund,
-    path: "/zyfund",
-    exact: true,
-  },
-  {
-    title: "资产",
-    component: ZyCapital,
-    path: "/zycapital",
-    exact: true,
-  },
-  {
-    title: "我的",
-    component: ZyMine,
-    path: "/zymine",
-    exact: true,
-  },
-  {
-    title: "登录",
-    component: Login,
-    path: "/login",
-    exact: true,
-  },
-  {
-    title: "notFund",
-    component: NotFound,
-    path: "",
-    exact: true,
-  },
-];
-function RouterConfig({ history }) {
+// import ZyHome from "./components/ZyHome/ZyHome";
+// import ZyFund from "./components/ZyFund/ZyFund";
+// import ZyCapital from "./components/ZyCapital/ZyCapital";
+// import ZyMine from "./components/ZyMine/ZyMine";
+// import NotFound from "./components/NotFound/NotFound";
+// import Login from "./components/Login/Login";
+
+function RouterConfig({ history, app }) {
+  const Counter = dynamic({
+    app,
+    component: () => import("./routes/Counter"),
+  });
+  const Main = dynamic({
+    app,
+    component: () => import("./routes/Main"),
+  });
+  const ZyHome = dynamic({
+    app,
+    component: () => import("./components/ZyHome/ZyHome"),
+  });
+  const ZyFund = dynamic({
+    app,
+    component: () => import("./components/ZyFund/ZyFund"),
+  });
+  const ZyCapital = dynamic({
+    app,
+    component: () => import("./components/ZyCapital/ZyCapital"),
+  });
+  const ZyMine = dynamic({
+    app,
+    component: () => import("./components/ZyMine/ZyMine"),
+  });
+  const NotFound = dynamic({
+    app,
+    component: () => import("./components/NotFound/NotFound"),
+  });
+  const Login = dynamic({
+    app,
+    component: () => import("./components/Login/Login"),
+  });
+
+  const routerList = [
+    {
+      title: "首页",
+      component: Counter,
+      path: "/counter",
+      exact: true,
+    },
+    {
+      title: "首页",
+      component: Main,
+      path: "/main",
+      exact: true,
+    },
+    {
+      title: "首页",
+      component: ZyHome,
+      path: "/zyhome",
+      exact: true,
+    },
+    {
+      title: "基金",
+      component: ZyFund,
+      path: "/zyfund",
+      exact: true,
+    },
+    {
+      title: "资产",
+      component: ZyCapital,
+      path: "/zycapital",
+      exact: true,
+    },
+    {
+      title: "我的",
+      component: ZyMine,
+      path: "/zymine",
+      exact: true,
+    },
+    {
+      title: "登录",
+      component: Login,
+      path: "/login",
+      exact: true,
+    },
+    {
+      title: "notFund",
+      component: NotFound,
+      path: "",
+      exact: true,
+    },
+  ];
+
   useEffect(() => {
     // 使用浏览器的 API 更新页面标题
     history.listen(({ pathname }) => {
@@ -60,6 +106,7 @@ function RouterConfig({ history }) {
       document.title = (item && item.title) || "中银基金";
     });
   });
+
   return (
     <Router history={history}>
       {/* <Suspense fallback={<div>Loading...</div>}> */}
