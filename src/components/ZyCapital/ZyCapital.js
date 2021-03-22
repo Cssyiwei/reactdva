ea8a4b374cad798f23176c7adfdac65d3a9895f7
cuiss <css@ect888.com>
2021-03-22 17:47:34 +0800

    资产页面头部开发完成

--- src/components/ZyCapital/ZyCapital.js
+++ src/components/ZyCapital/ZyCapital.js
@@ -1,18 +1,157 @@
 import React from "react";
 import { connect } from "dva";
 import TabBar from "../TabBar/TabBar";
-
+import { qlzccx } from "../../services/example.js";
+import { calMoneyQuantile, toFixed } from "../../utils/common.js";
+import style from "./zycapital.less";
 class ZyCapital extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userInfo: {},
+      isLook:
+        localStorage.isLook === undefined
+          ? true
+          : localStorage.isLook === "true"
+          ? true
+          : false,
+    };
   }
+  handleQlzccx() {
+    let custNo = localStorage.custNo;
+    qlzccx({ custNo }).then((data) => {
+      if (data.isSuccess === "0") {
+        data.content.totalasset1 = calMoneyQuantile(
+          toFixed(data.content.totalasset1, 2)
+        );
+
+        data.content.totalIncome1 = calMoneyQuantile(
+          toFixed(data.content.totalIncome1, 2)
+        );
+        data.content.totalIncome1 = data.content.totalIncome1.includes("-")
+          ? data.content.totalIncome1
+          : "+" + data.content.totalIncome1;
+
+        data.content.totalIncome2 = calMoneyQuantile(
+          toFixed(data.content.totalIncome2, 2)
+        );
+        data.content.totalIncome2 = data.content.totalIncome2.includes("-")
+          ? data.content.totalIncome2
+          : "+" + data.content.totalIncome2;
+
+        data.content.totalIncome3 = calMoneyQuantile(
+          toFixed(data.content.totalIncome3, 2)
+        );
+        data.content.totalIncome3 = data.content.totalIncome3.includes("-")
+          ? data.content.totalIncome3
+          : "+" + data.content.totalIncome3;
+
+        data.content.dayIncome1 = calMoneyQuantile(
+          toFixed(data.content.dayIncome1, 2)
+        );
+        data.content.dayIncome1 = data.content.dayIncome1.includes("-")
+          ? data.content.dayIncome1
+          : "+" + data.content.dayIncome1;
+
+        data.content.dayIncome2 = calMoneyQuantile(
+          toFixed(data.content.dayIncome2, 2)
+        );
+        data.content.dayIncome2 = data.content.dayIncome2.includes("-")
+          ? data.content.dayIncome2
+          : "+" + data.content.dayIncome2;
+
+        data.content.totalIncome3 = calMoneyQuantile(
+          toFixed(data.content.totalIncome3, 2)
+        );
+        data.content.totalIncome3 = data.content.totalIncome3.includes("-")
+          ? data.content.totalIncome3
+          : "+" + data.content.totalIncome3;
+
+        data.content.totalShare = calMoneyQuantile(
+          toFixed(data.content.totalShare, 2)
+        );
+        data.content.totalasset4 = calMoneyQuantile(
+          toFixed(data.content.totalasset4, 2)
+        );
+        data.content.dayIncome4 = calMoneyQuantile(
+          toFixed(data.content.dayIncome4, 2)
+        );
+        data.content.totalIncome4 = calMoneyQuantile(
+          toFixed(data.content.totalIncome4, 2)
+        );
+        data.content.totalasset3 = calMoneyQuantile(
+          toFixed(data.content.totalasset3, 2)
+        );
+        data.content.totalasset1 = calMoneyQuantile(
+          toFixed(data.content.totalasset1, 2)
+        );
+        // if (localStorage.isLook === undefined) {
+        //   this.setState({
+        //     isLook: true,
+        //   });
+        //   localStorage.setItem("isLook", true);
+        // }
+        this.setState({ userInfo: data.content });
+      }
+    });
+  }
+  toggleEye() {
+    localStorage.setItem("isLook", !this.state.isLook);
+    this.setState({
+      isLook: !this.state.isLook,
+    });
+  }
+  componentDidMount() {
+    this.handleQlzccx();
+  }
+
+  header() {
+    return (
+      <header className="color-fff align-center">
+        <div className="flex-between" style={{ padding: "0.3rem 0.2rem 0" }}>
+          <span className="updateTime">最后更新于:updateDate</span>
+          <div>
+            <img
+              className="message"
+              src={require("../../assets/position/icon_message_write.png")}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="total ta-c">
+          <span className="ft-20">
+            总资产(元)
+            <i
+              className={!this.state.isLook ? "eye-off" : ""}
+              onClick={() => this.toggleEye()}
+            ></i>
+          </span>
+          <p>{this.state.isLook ? this.state.userInfo.totalasset1 : "****"}</p>
+        </div>
+        <div className="income flex ta-c" ref="income">
+          <dl className="left flex-1">
+            <dd>最新收益(元)</dd>
+            <dt>
+              {this.state.isLook ? this.state.userInfo.dayIncome1 : "****"}
+            </dt>
+          </dl>
+          <dl className="flex-1">
+            <dd>累计收益(元)</dd>
+            <dt>
+              {this.state.isLook ? this.state.userInfo.totalIncome1 : "****"}
+            </dt>
+          </dl>
+        </div>
+      </header>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <h1>ZyCapital</h1>
+      <main className={style.zycapital}>
+        {this.header()}
         <TabBar history={this.props.history} match={this.props.match} />
-      </div>
+      </main>
     );
   }
 }
---
+++ src/utils/common.js
@@ -0,0 +1,77 @@
+export function convertInc(num) {
+  if (num.indexOf("-") > -1 || num === "" || num === "-") {
+    return -1;
+  }
+  return 1;
+}
+
+/**
+   * 金额添加分位符: parseInt toFixed计算过的number值, 是否需要小数位
+   */
+export function calMoneyQuantile(number, hasPoint = true) {
+  if (number === undefined || isNaN(number)) {
+    return 0;
+  }
+  let arr = [];
+  if ((number + '').indexOf('+') === 0) {
+    arr = (number + '').split('+');
+    arr[0] = '+';
+  } else if ((number + '').indexOf('-') === 0) {
+    arr = (number + '').split('-');
+    arr[0] = '-';
+  } else {
+    arr = ['', number + ''];
+  }
+  if (arr[1].length < 4) {
+    return hasPoint ? arr[0] + arr[1] + '.00' : arr[0] + arr[1];
+  }
+  const pointNum = arr[1].indexOf('.') < 0 ? '' : arr[1].substr(arr[1].indexOf('.') + 1, arr[1].length);
+  let num = pointNum === '' ? arr[1] : arr[1].substr(0, arr[1].length - pointNum.length - 1);
+  let result = '';
+  while (num.length > 3) {
+    result = ',' + num.slice(-3) + result;
+    num = num.slice(0, num.length - 3);
+  }
+  if (hasPoint) {
+    switch (pointNum.length) {
+      case 0:
+        return arr[0] + num + result + '.00';
+      case 1:
+        return arr[0] + num + result + '.' + pointNum + '0';
+      case 2:
+      default:
+        return arr[0] + num + result + '.' + pointNum;
+    }
+  }
+  return arr[0] + num + result;
+}
+
+export function toFixed(number, length, hasPer = false) {
+  if (number === undefined || isNaN(number)) {
+    return '--';
+  }
+  const numberPlus = number < 0 ? number * (-1) : number; // 负数做正数四舍五入后，再输出负数
+  const multiple = 10 ** length; // multiple为10的length次方 设置浮点数要扩大的倍数
+  const str = numberPlus + '';
+  let dot = str.indexOf('.');
+  // 找到要进行舍入的数的位置，手动判断是否大于等于5，满足条件进位标志置为1(存放进位标志)
+  // num为原浮点数放大multiple倍后的数, 去掉舍入位后的所有数，然后加上我们的手动进位数
+  // 失精!!! 当前 9.04 * 100 = 903.9999999??
+  const num = dot > -1 && str.substr(dot + length + 1, 1) >= 5 ? Math.round(numberPlus * multiple) + 1 : Math.round(numberPlus * multiple);
+  // const num = dot > -1 && str.substr(dot + length + 1, 1) >= 5 ? Math.floor(numberPlus * multiple) + 1 : Math.floor(numberPlus * multiple);
+  let result = num / multiple + ''; // 将进位后的整数再缩小为原浮点数
+  // 处理进位后无小数
+  if (result.indexOf('.') < 0) {
+    result += '.';
+  }
+  dot = result.indexOf('.');
+  // 处理多次进位、保留length长度的小数
+  const len = result.length - (dot + 1);
+  if (len < length) {
+    for (let i = 0; i < length - len; i++) {
+      result += 0;
+    }
+  }
+  const resultStr = number < 0 ? '-' + result : result;
+  return hasPer ? resultStr + '%' : resultStr;
+};
